de01d3685bd7f41bbb6b50651eafb2f1e1501c13
Phoenix Song <phoenixsong@outlook.com>
2018-06-24 13:12:19 -0700

    build script fix

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
-mkdir 0.0.5
-GOOS=linux GOARCH=amd64 go build -o ./0.0.5/salienbot-0.0.5-linux-x64 .
-GOOS=linux GOARCH=386 go build -o ./0.0.5/salienbot-0.0.5-linux-x86 .
-GOOS=darwin GOARCH=amd64 go build -o ./0.0.5/salienbot-0.0.5-darwin .
-GOOS=windows GOARCH=amd64 go build -o ./0.0.5/salienbot-0.0.5-win64.exe .
-GOOS=windows GOARCH=386 go build -o ./0.0.5/salienbot-0.0.5-win32.exe .
-GOOS=linux GOARCH=arm go build -o ./0.0.5/salienbot-0.0.5-linux-arm .
-GOOS=linux GOARCH=arm64 go build -o ./0.0.5/salienbot-0.0.5-linux-arm64 .
+#!/bin/bash
+
+# Use with `VER=0.0.X ./build.sh`
+[ -z "$VER" ] && echo "Please use with a version env VER=0.0.x" && exit 1
+
+rm -rf ./$VER
+mkdir $VER
+GOOS=linux GOARCH=amd64 go build -o ./$VER/salienbot-$VER-linux-x64 .
+GOOS=linux GOARCH=386 go build -o ./$VER/salienbot-$VER-linux-x86 .
+GOOS=darwin GOARCH=amd64 go build -o ./$VER/salienbot-$VER-darwin .
+GOOS=windows GOARCH=amd64 go build -o ./$VER/salienbot-$VER-win64.exe .
+GOOS=windows GOARCH=386 go build -o ./$VER/salienbot-$VER-win32.exe .
+GOOS=linux GOARCH=arm go build -o ./$VER/salienbot-$VER-linux-arm .
+GOOS=linux GOARCH=arm64 go build -o ./$VER/salienbot-$VER-linux-arm64 .
 
 docker build -t azusa0127/salienbot-go . && docker push azusa0127/salienbot-go
