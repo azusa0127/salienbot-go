ea9f3f0699f7b15f7b74cab9631c648a5ccc8ae3
Phoenix.S <PhoenixSong@Outlook.com>
2018-06-29 00:48:09 -0700

    version update

--- main.go
+++ main.go
@@ -484,7 +484,7 @@ func main() {
 	}
 	errc := make(chan error)
 	go func() {
-		log.Println("[SalienBot] 0.2.0 Listening to terminate signal ctrl-c...")
+		log.Println("[SalienBot] 0.2.1 Listening to terminate signal ctrl-c...")
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		errc <- fmt.Errorf("Signal %v", <-c)
@@ -499,5 +499,5 @@ func main() {
 		time.Sleep(3 * time.Second)
 	}
 
-	log.Println("[SalienBot] 0.2.0 Terminated - ", <-errc)
+	log.Println("[SalienBot] 0.2.1 Terminated - ", <-errc)
 }
