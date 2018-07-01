a77a18189979e98a95947c668cf5b46ddcf05715
Phoenix.S <PhoenixSong@Outlook.com>
2018-07-01 14:28:27 -0700

    betaver build

--- buildbeta.sh
+++ buildbeta.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Use with `VER=0.0.X ./build.sh`
-[ -z "$VER-beta" ] && echo "Please use with a version env VER=0.0.x" && exit 1
+[ -z "$BETAVER" ] && echo "Please use with a version env BETAVER=0.0.x-beta" && exit 1
 
-rm -rf ./$VER-beta
-mkdir $VER-beta
-GOOS=linux GOARCH=amd64 go build -o ./$VER-beta/salienbot-$VER-beta-linux-x64 .
-GOOS=linux GOARCH=386 go build -o ./$VER-beta/salienbot-$VER-beta-linux-x86 .
-GOOS=darwin GOARCH=amd64 go build -o ./$VER-beta/salienbot-$VER-beta-darwin .
-GOOS=windows GOARCH=amd64 go build -o ./$VER-beta/salienbot-$VER-beta-win64.exe .
-GOOS=windows GOARCH=386 go build -o ./$VER-beta/salienbot-$VER-beta-win32.exe .
-GOOS=linux GOARCH=arm go build -o ./$VER-beta/salienbot-$VER-beta-linux-arm .
-GOOS=linux GOARCH=arm64 go build -o ./$VER-beta/salienbot-$VER-beta-linux-arm64 .
+rm -rf ./$BETAVER
+mkdir $BETAVER
+GOOS=linux GOARCH=amd64 go build -o ./$BETAVER/salienbot-$BETAVER-linux-x64 .
+GOOS=linux GOARCH=386 go build -o ./$BETAVER/salienbot-$BETAVER-linux-x86 .
+GOOS=darwin GOARCH=amd64 go build -o ./$BETAVER/salienbot-$BETAVER-darwin .
+GOOS=windows GOARCH=amd64 go build -o ./$BETAVER/salienbot-$BETAVER-win64.exe .
+GOOS=windows GOARCH=386 go build -o ./$BETAVER/salienbot-$BETAVER-win32.exe .
+GOOS=linux GOARCH=arm go build -o ./$BETAVER/salienbot-$BETAVER-linux-arm .
+GOOS=linux GOARCH=arm64 go build -o ./$BETAVER/salienbot-$BETAVER-linux-arm64 .
 
-docker build --label "Version=$VER-beta" -t azusa0127/salienbot-go:$VER-beta . && docker push azusa0127/salienbot-go:$VER-beta
-docker build --label "Version=$VER-beta" -t azusa0127/salienbot-go:latest .
+docker build --label "Version=$BETAVER" -t azusa0127/salienbot-go:$BETAVER . && docker push azusa0127/salienbot-go:$BETAVER
+docker build --label "Version=$BETAVER" -t azusa0127/salienbot-go:latest .
